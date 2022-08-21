7d23bdd0a83b898b899a1dc8300b9a7d5f200478
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2022-08-22 03:25:59 +0600

    create selection players fild using js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const cart = [];
 
 
+function displayPlayers(){
+    const totalPlayers = document.getElementById('total-players');
+    totalPlayers.innerText = cart.length;
+
+    const selectedPlayer = document.getElementById('collection-players');
+    selectedPlayer.textContent = '';
+
+
+    for(let i=0; i<cart.length; i++ ){
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <th>${i+1}</th>
+        <td>${cart[i].playerName}</td>
+        `
+        selectedPlayer.appendChild(tr);
+    }
+
+}
 function player(element){
     const playerName = element.parentNode.parentNode.children[0].innerText;
   
@@ -9,12 +27,5 @@ function player(element){
     playerName:playerName,
    }
    cart.push(players);
-   console.log(players);
-}
-
-function displayPlayers(){
-    const totalPlayers = document.getElementById('total-players');
-    totalPlayers.innerText = cart.length;
-
-    const selectedPlayer = document.getElementById('collection-players');
+   displayPlayers();
 }
